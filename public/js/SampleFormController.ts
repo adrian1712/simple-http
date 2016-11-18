d1f5d13d3e8a22772c6b57432f0ba82178768cb5
Adrian Amaya <aa17.1atq@gmail.com>
2016-11-18 14:46:11 -0700

    added log in form

--- public/js/SampleFormController.ts
+++ public/js/SampleFormController.ts
@@ -1,5 +1,26 @@
 namespace App {
     export class SampleFormController{
+        public fullName
+        public userName
+        public email
+
+        constructor () {
+            this.fullName = ""
+            this.userName = ""
+            this.email = ""
+        }
+
+
+        public saveUser (userRegister){
+            console.log ("hello")
+            console.log ("form object", userRegister)
+            if (userRegister.$valid == false) {
+                alert ("please fill out form corectly")
+            }
+            else {
+                console.log ('submiting new user');
+            }
+        }
 
     }
 }
